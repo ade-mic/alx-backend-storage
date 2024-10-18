1ed52ab7d7a35abf9b0e0b2a0446408ba1c371af
ade-mic <ademic.aina@gmail.com>
2024-10-18 15:23:44 +0100

    task 7, 8, 9, 10 and 11

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,4 +1,7 @@
--- Assuming the table names are 'users', 'corrections', and 'average_scores'
+--  a SQL script that creates a stored procedure
+--ComputeAverageScoreForUser that computes and
+-- store the average score for a student.
+-- Note: An average score can be a decimal
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
@@ -12,10 +15,10 @@ BEGIN
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Insert or update the average score in the average_scores table
-    INSERT INTO average_scores (user_id, average_score)
-    VALUES (user_id, avg_score)
-    ON DUPLICATE KEY UPDATE average_score = avg_score;
+    -- Update the average score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
 END //
 
 DELIMITER ;
