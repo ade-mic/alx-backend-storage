5506839edc5b980eca2042e846bd9353ed4efafd
ade-mic <ademic.aina@gmail.com>
2024-10-21 20:57:34 +0100

    text

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
 """
-a Python script that provides some stats about Nginx logs
-stored in MongoDB:
-
-Database: logs
-Collection: nginx
-Display (same as the example):
-first line: x logs where x is the number of documents in this
-collection
-second line: Methods:
-5 lines with the number of documents with
-the method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-in this order (see example below - warning: it’s a tabulation
-before each line)
-one line with the number of documents with:
-method=GET
-path=/status
+This script provides statistics about Nginx logs stored in MongoDB.
 """
+
 from pymongo import MongoClient
-if __name__ == "__main__":
+
+def nginx_stats():
+    """
+    Provides statistics about Nginx logs stored in MongoDB.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    ngix_collection = client.logs.nginx
-    print(f"{ngix_collection.count_documents({})} logs\nMethods: \n \t method GET: {ngix_collection.count_documents({'method': 'GET'})} \
-            \n \t method POST: {ngix_collection.count_documents({'method': 'POST'})}\
-            \n \t method PUT: {ngix_collection.count_documents({'method': 'PUT'})}\
-            \n \t method PATCH: {ngix_collection.count_documents({'method': 'PATCH'})}\
-            \n \t method DELETE: {ngix_collection.count_documents({'method': 'DELETE'})}\
-            \n {ngix_collection.count_documents({'path': '/status'})} status check")
+    db = client.logs
+    collection = db.nginx
+
+    # Total number of logs
+    total_logs = collection.count_documents({})
+
+    # Number of logs for each HTTP method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: collection.count_documents({"method": method}) for method in methods}
+
+    # Number of logs with method=GET and path=/status
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+
+    # Display the statistics
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    nginx_stats()
