0da31f18e4f676681795827ccae02bbe3ee0e0a8
ade-mic <ademic.aina@gmail.com>
2024-10-23 04:52:16 +0100

    102-log_stats.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python3
+"""
+    Aggregation pipeline to calculate
+    the average score and sort by it
+"""
 from pymongo import MongoClient
 
+
 def top_students(mongo_collection):
-    # Aggregation pipeline to calculate the average score and sort by it
+    """
+    Aggregation pipeline to calculate
+    the average score and sort by it
+    """
     pipeline = [
         {
             '$project': {
                 'name': 1,
-                'averageScore': { '$avg': '$scores.score' }
+                'averageScore': {'$avg': '$topics.score'}
             }
         },
         {
-            '$sort': { 'averageScore': -1 }
+            '$sort': {'averageScore': -1}
         }
     ]
-    
+
     # Execute the aggregation pipeline
     return list(mongo_collection.aggregate(pipeline))
---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+This script provides statistics about Nginx logs stored in MongoDB.
+"""
+
+from pymongo import MongoClient
+
+
+def nginx_stats():
+    """
+    Provides statistics about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    # Total number of logs
+    total_logs = collection.count_documents({})
+
+    # Number of logs for each HTTP method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: collection.count_documents({"method": method})
+                     for method in methods}
+
+    # Number of logs with method=GET and path=/status
+    status_check = collection.count_documents({"method": "GET",
+                                               "path": "/status"})
+
+    # Top 10 most present IPs
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    # Display the statistics
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check} status check")
+
+    print("Top 10 IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    nginx_stats()
